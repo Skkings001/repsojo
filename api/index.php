f6ac092d72761a316d027997d975c950a2d5a8f4
Skkings001 <skpgamer001@outlook.com>
2024-10-01 00:46:20 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,27 @@
 <?php
 
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+$url = 'https://babel-in.xyz/tata/channels';
+$data = [
+    'X-API-Key' => 'babel-40b84ca520bd0fe134cc01fc80',
+];
+$jsonData = json_encode($data);
+
+$options = [
+    'http' => [
+        'header'  => [
+            "Content-type: application/json",
+            "User-Agent: Babel/5.0"
+        ],
+        'method'  => 'POST',
+        'content' => $jsonData,
+    ],
+];
+
+$context  = stream_context_create($options);
+$response = file_get_contents($url, false, $context);
+
+if ($response === FALSE) {
+    die('Error sending request');
+}
+
+echo $response;
